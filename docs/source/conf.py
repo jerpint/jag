6c1b6774c26ba64f7c51b1b9b341f0cd804cbb61
Gaetan Marceau Caron <gaetan.marceau.caron@mila.quebec>
2019-05-22 18:05:46 -0400

    Doing more test with Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,9 @@ author = 'Jeremy Pinto, Arsene Fansi Tchango, Gaetan Marceau Caron'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+        'sphinx.ext.autodoc',
+        'sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
