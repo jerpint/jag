b6de1be165fcb5b39c6515558767222e5724fee1
Jeremy Pinto <jerpint@gmail.com>
2019-05-27 10:36:39 -0400

    Update fasttext function, add docstrings

--- utils/fasttext.py
+++ utils/fasttext.py
@@ -3,7 +3,15 @@ import numpy as np
 
 
 class FasttextEmbedding:
+    """Fasttext class to get word embeddingsline.
 
+    Extended description of function.
+
+    Args:
+        fname (str): name of the file containing fasttext tokens
+        max_tokens (int): Max number of tokens to load in memory
+
+    """
     def __init__(self, fname, max_tokens=1e4):
 
         self.fname = fname
@@ -13,15 +21,11 @@ class FasttextEmbedding:
     def load_fasttext_embeddings(self):
         '''
         Loads fasttext word embeddings in memory
-
-
         '''
-        fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-        n, d = map(int, fin.readline().split())
+        filein = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
         data = {}
-        for count, line in enumerate(fin):
+        for count, line in enumerate(filein):
             tokens = line.rstrip().split(' ')
-            #  data[tokens[0].lower()] = map(float, tokens[1:])
             data[tokens[0].lower()] = np.asarray(tokens[1:]).astype('float32')
 
             if count > self.max_tokens:
@@ -43,3 +47,4 @@ if __name__ == "__main__":
 
     fname = 'data/wiki-news-300d-1M.vec'
     embeddings = FasttextEmbedding(fname)
+    print(embeddings.get_word('cool'))
