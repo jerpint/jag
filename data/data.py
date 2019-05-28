8d0cc2d708f76d2e18860f3fd318edb50f43b227
Jeremy Pinto <jerpint@gmail.com>
2019-05-28 10:46:41 -0400

    Add batching to dataloader

--- data/data.py
+++ data/data.py
@@ -1,8 +1,51 @@
 import gzip
 import json
 
+import io
+import numpy as np
 
-def dataset_generator(fname='data/SQuAD.jsonl.gz'):
+
+class FasttextEmbedding:
+    """Fasttext class to get word embeddings.
+
+    Extended description of function.
+
+    Args:
+        fname (str): name of the file containing fasttext tokens
+        max_tokens (int): Max number of tokens to load in memory
+
+    """
+    def __init__(self, fname, max_tokens=1e4):
+
+        self.fname = fname
+        self.max_tokens = max_tokens
+        self.load_fasttext_embeddings()
+
+    def load_fasttext_embeddings(self):
+        '''
+        Loads fasttext word embeddings in memory
+        '''
+        filein = io.open(self.fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        data = {}
+        for count, line in enumerate(filein):
+            tokens = line.rstrip().split(' ')
+            data[tokens[0].lower()] = np.asarray(tokens[1:]).astype('float32')
+
+            if count > self.max_tokens:
+                break
+        self.embeddings = data
+
+    def embed(self, word):
+
+        word = word.lower()
+
+        if word in self.embeddings.keys():
+            return self.embeddings[word]
+        else:
+            return np.random.random(300)
+
+
+def dataset_generator(fname='data/SQuAD.jsonl.gz', batch_size=4):
     """Simple dataset generator for MRQA.
 
     Extended description of function.
@@ -15,13 +58,52 @@ def dataset_generator(fname='data/SQuAD.jsonl.gz'):
 
     """
 
+    # Load fasttext embeddings
+    ftext = FasttextEmbedding(fname='data/wiki-news-300d-1M.vec')
+
     with gzip.open(fname, 'rb') as f:
+
+        batch_context_embeddings = []
+        batch_context_positions = []
+        batch_questions = []
+
         for i, line in enumerate(f):
-            obj = json.loads(line)
+            instance = json.loads(line)
+            print("Here")
 
             # Skip headers.
-            if i == 0 and 'header' in obj:
+            if i == 0 and 'header' in instance:
+                count = 0
                 continue
 
             else:
-                yield obj
+                if count < batch_size:
+
+                    context_tokens = instance['context_tokens']
+                    context_embeddings = []
+                    context_positions = []
+                    questions = instance['qas']
+
+                    for token, pos in context_tokens:
+                        context_embeddings.append(ftext.embed(token))
+                        context_positions.append(pos)
+
+                    context_embeddings = np.asarray(context_embeddings)
+                    context_embeddings = np.expand_dims(context_embeddings, axis=1)
+
+                    batch_context_embeddings.append(context_embeddings)
+                    batch_context_positions.append(context_positions)
+                    batch_questions.append(questions)
+
+                    count += 1
+                else:
+
+                    yield (batch_context_embeddings,
+                           batch_context_positions,
+                           batch_questions)
+
+                    batch_context_embeddings = []
+                    batch_context_positions = []
+                    batch_questions = []
+
+                    count = 0
