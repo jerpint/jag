d79d887021d524e73790fd0c14d3a08025c71cdc
Gaetan Marceau Caron <gaetan.marceau.caron@mila.quebec>
2019-06-05 19:25:47 -0400

    using the sample data source file

--- unit_tests/data_fetcher_test.py
+++ unit_tests/data_fetcher_test.py
@@ -2,5 +2,5 @@ import utils.data_fetcher
 
 
 def test_mrqa_text_fetcher():
-    utils.data_fetcher.get_file(data_src='./data/mrqa_urls.txt',
+    utils.data_fetcher.get_file(data_src='./data/mrqa_urls_sample.txt',
                                 cache_dir='./unit_tests/data')
