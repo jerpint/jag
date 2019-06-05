08fd2b9bd06d2ebbbb316d727d18f8ce42e94497
Jeremy Pinto <jerpint@gmail.com>
2019-06-05 16:01:31 -0400

    Add unit test for dataset generator

--- unit_tests/unit_test.py
+++ unit_tests/unit_test.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import numpy as np
+
+from data.data import dataset_generator
 
 
 def test_tensorflow():
@@ -12,3 +15,25 @@ def test_tensorflow():
         out = sess.run(graph)
 
     assert out == 7
+
+
+def test_generator():
+
+    batch_size = 2
+
+    data = dataset_generator(batch_size=batch_size)
+    context_embeddings, context_positions, questions = next(data)
+
+    assert len(context_embeddings) == batch_size
+    assert len(context_positions) == batch_size
+    assert len(questions) == batch_size
+
+    for ce in context_embeddings:
+        assert type(ce) == np.ndarray
+
+    for cp in context_positions:
+        assert type(cp) == list
+
+    for qs in questions:
+        keys = ['answers', 'question', 'id', 'qid', 'question_tokens', 'detected_answers']
+        assert (keys == list(qs[0].keys()))
