d27bb78798edc8094287096d7bfd3bd8e6a1f713
Jeremy Pinto <jerpint@gmail.com>
2019-06-07 14:26:10 -0400

    Update tests to work with jag package

--- unit_tests/data_fetcher_test.py
+++ unit_tests/data_fetcher_test.py
@@ -1,6 +1,6 @@
-import utils.data_fetcher
+from jag.utils import data_fetcher
 
 
 def test_mrqa_text_fetcher():
-    utils.data_fetcher.get_file(data_src='./data/mrqa_urls_sample.txt',
-                                cache_dir='./unit_tests/data')
+    data_fetcher.get_file(data_src='./jag/data/mrqa_urls_sample.txt',
+                          cache_dir='./unit_tests/data')
