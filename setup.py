805e53a29d8464a19fb0b980dc30ac7952ae58b4
Jeremy Pinto <jerpint@gmail.com>
2019-06-07 14:12:09 -0400

    Change project name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ecodse-funtime-alpha',
+    name='jag',
     python_requires='>=3.6',
     version='0.1a',
     description='Jag for MRQA',
